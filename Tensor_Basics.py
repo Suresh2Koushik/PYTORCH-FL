379c0868ba9eaa475b45b4cb15c90b7688514732
Suresh Kanna <81513268+Suresh2Koushik@users.noreply.github.com>
2021-07-06 23:55:03 +0530

    Create Tensor_Basics.py

---
+++ Tensor_Basics.py
@@ -0,0 +1,174 @@
+# Everything in pytorch is based on Tensor operations.
+# A tensor can have different dimensions
+# so it can be 1d, 2d, or even 3d and higher
+
+# scalar, vector, matrix, tensor
+
+# torch.empty(size): uninitiallized
+
+x = torch.empty(1) # scalar
+print(x)
+x = torch.empty(3) # vector, 1D
+print(x)
+x = torch.empty(2,3) # matrix, 2D
+print(x)
+x = torch.empty(2,2,3) # tensor, 3 dimensions
+#x = torch.empty(2,2,2,3) # tensor, 4 dimensions
+print(x)
+
+# torch.rand(size): random numbers [0, 1]
+x = torch.rand(5, 3)
+print(x)
+
+# torch.zeros(size), fill with 0
+# torch.ones(size), fill with 1
+x = torch.zeros(5, 3)
+print(x)
+
+y=torch.ones(1,4)
+print(y)
+
+# check size
+# Everything in pytorch is based on Tensor operations.
+# A tensor can have different dimensions
+# so it can be 1d, 2d, or even 3d and higher
+
+# scalar, vector, matrix, tensor
+
+# torch.empty(size): uninitiallized
+x = torch.empty(1) # scalar
+print(x)
+x = torch.empty(3) # vector, 1D
+print(x)
+x = torch.empty(2,3) # matrix, 2D
+print(x)
+x = torch.empty(2,2,3) # tensor, 3 dimensions
+#x = torch.empty(2,2,2,3) # tensor, 4 dimensions
+print(x)
+
+# torch.rand(size): random numbers [0, 1]
+x = torch.rand(5, 3)
+print(x)
+
+# torch.zeros(size), fill with 0
+# torch.ones(size), fill with 1
+x = torch.zeros(5, 3)
+print(x)
+
+# check size
+print(x.size())
+
+# check data type
+print(x.dtype)
+
+# specify types, float32 default
+x = torch.zeros(5, 3, dtype=torch.float16)
+print(x)
+# Everything in pytorch is based on Tensor operations.
+# A tensor can have different dimensions
+# so it can be 1d, 2d, or even 3d and higher
+
+# scalar, vector, matrix, tensor
+
+# torch.empty(size): uninitiallized
+x = torch.empty(1) # scalar
+print(x)
+x = torch.empty(3) # vector, 1D
+print(x)
+x = torch.empty(2,3) # matrix, 2D
+print(x)
+x = torch.empty(2,2,3) # tensor, 3 dimensions
+#x = torch.empty(2,2,2,3) # tensor, 4 dimensions
+print(x)
+
+# torch.rand(size): random numbers [0, 1]
+x = torch.rand(5, 3)
+print(x)
+
+# torch.zeros(size), fill with 0
+# torch.ones(size), fill with 1
+x = torch.zeros(5, 3)
+print(x)
+
+y = torch.ones(5, 3)
+print(y)
+
+
+# check size
+print(x.size())
+print(y.size())
+
+# check data type
+print(x.dtype)
+print(y.dtype)
+
+# specify types, float32 default
+x = torch.zeros(5, 3, dtype=torch.float16)
+print(x)
+
+# check type
+print(x.dtype)
+# check type
+print(x.dtype)
+# check data type
+print(x.dtype)
+
+# specify types, float32 default
+x = torch.zeros(5, 3, dtype=torch.float16)
+print(x)
+
+# check type
+print(x.dtype)
+
+
+#simple addition
+
+#x-input random numbers
+x=torch.rand(2,2)
+print(x)
+
+#y-input random numbers
+y=torch.rand(2,2)
+print(y)
+
+#add- using symbol
+a=x+y
+
+print(a)
+
+#add- using function
+b=torch.add(x,y)
+print(b)
+
+#inplace functions's operation
+#inpalce--underscore
+y.add_(x)
+print(y)
+
+#basic maths operation:-
+#using builtin function
+
+b=torch.add(x,y)
+print(b)
+
+c=torch.sub(x,y)
+print(c)
+
+c=torch.mul(x,y)
+print(c)
+
+d=torch.div(x,y)
+print(d)
+
+
+# Slicing
+x = torch.rand(5,3)
+print(x)
+print(x[:, 0]) # all rows, column 0
+print(x[1, :]) # row 1, all columns
+print(x[1,1]) # element at 1, 1
+print(x[2,2])
+print(x[4,2])
+# Get the actual value if only 1 element in your tensor
+print(x[1,1].item())
+
