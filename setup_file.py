ff5aa945a1e187096344453e2e71afad202ae173
Suresh Kanna <81513268+Suresh2Koushik@users.noreply.github.com>
2021-07-06 22:49:51 +0530

    Update setup_file.py

--- setup_file.py
+++ setup_file.py
@@ -1,1 +1,16 @@
+#I have used google collab as a IDE to leran about pytorch
+#Refer this link to install pytorch in the google collab
+
+https://stackoverflow.com/questions/53775508/how-do-i-install-pytorch-v1-0-0-on-google-colab
+
+ #to install the PYtorch, run thr follwing commands
+
+!pip install torch
+!pip install torchvision
+!pip install mxnet-cu101
+
+#checking the version of the pytorch
+
+import torch
+print(torch.__version__)
 
